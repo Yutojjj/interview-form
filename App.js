2722b83c31da805baba241b7a64287f6d69787f1
Yutojjj <100813506+Yutojjj@users.noreply.github.com>
2026-03-05 06:34:35 +0900

    Update App.js

--- App.js
+++ App.js
@@ -243,7 +243,7 @@ export default function App() {
     setIsSubmitting(true);
     try {
       // ⚠️ あなたのGASのURLに書き換えてください
-      const GAS_URL = "https://script.google.com/macros/s/AKfycbwe4yl5he09Fsb_a8WtMV0PZX36q0VxIyPZK0c1X0EMHkC6ZCr9fLrPFRjd2ZJNt9yKVQ/exec"; 
+      const GAS_URL = "https://script.google.com/macros/s/AKfycbzLk3gLAd9UOAGzpqGn_5QQCSOfy7McpkQ5Li8yCVhDizNT2l4GEoL9NcHkeScVhvkD5g/exec"; 
       const searchParams = new URLSearchParams();
       Object.keys(form).forEach(key => {
         if (Array.isArray(form[key])) { searchParams.append(key, form[key].join(', ')); } else { searchParams.append(key, form[key]); }
